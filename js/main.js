494602036fe342c2690b4eebfa17b8934df00750
Sergey <redmoloch@yanadex.ru>
2021-05-29 14:22:02 +0300

    added getRandomInt

---
+++ js/main.js
@@ -0,0 +1,45 @@
+// Функция, возвращающая случайное целое число из переданного диапазона включительно:
+
+const getRandomInt = (min = 1, max = 10) => {
+  if (max < min) {
+    [max, min] = [min, max];
+  }
+
+  if (max < 0) { // блок можно удалить
+    return 0;
+  }
+
+  if (max === min) {
+    return min;
+  }
+
+  min = min < 0 ? 0 : min; // блок можно удалить
+
+  return Math.round(Math.random() * (max - min) + min);
+};
+
+
+// Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
+// можно заменнить ей и getRandomInt
+
+const getRandomNumber = (min = 1, max = 10, afterPoint = 0) => {
+  if (max < min) {
+    [max, min] = [min, max];
+  }
+
+  if (max < 0) { // блок можно удалить
+    return 0;
+  }
+
+  if (max === min) {
+    return min;
+  }
+
+  min = min < 0 ? 0 : min; // блок можно удалить
+
+  return +(Math.random() * (max - min) + min).toFixed(afterPoint);
+};
+
+
+getRandomInt(1, 2);
+getRandomNumber(1, 2, 3);
