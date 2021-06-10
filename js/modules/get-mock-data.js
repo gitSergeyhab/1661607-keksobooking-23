06cedb57c12898e639af6aa6eabd25b02764ba8c
Sergey <redmoloch@yandex.ru>
2021-06-10 18:31:37 +0300

    made data.js

--- js/modules/get-mock-data.js
+++ js/modules/get-mock-data.js
@@ -1,27 +1,8 @@
 import {getRandomInt, getRandomNumber} from './util.js';
+import {START_LAT, FINISH_LAT, START_LNG, FINISH_LNG, AFTER_POINT, PHOTO_COUNT, MIN_PRICE, MAX_PRICE, MAX_ROOMS, MAX_GUESTS, types, times, protoFeatures, protoPhotos} from './data.js';
 
 const getMockData = (offerCount) => {
-  // если эти функции и "переменные"  будут использоваться где-то еще, вынесу в отдельный модуль, а пока так:
-  const START_LAT = 35.65000;
-  const FINISH_LAT = 35.70000;
-  const START_LNG = 139.70000;
-  const FINISH_LNG = 139.80000;
-  const AFTER_POINT = 5;
-  const PHOTO_COUNT = 7;
-  const MIN_PRICE = 1000;
-  const MAX_PRICE = 100000;
-  const MAX_ROOMS = 10;
-  const MAX_GUESTS = 1000;
-
-  const types = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
-  const times = ['12:00', '13:00', '14:00'];
-  const protoFeatures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-  const protoPhotos = [
-    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
-    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
-  ];
-
+  // если эти функции будут использоваться где-то еще, вынесу в отдельный модуль, а пока так:
   const getAvatar = (index) => `img/avatars/user0${index + 1}.png`;
   const getTitle = (index) => `offer-title-${index + 1}`;
   const getDescription = (index) => `чрезвычайно обычное помещение № ${index + 1}`;
