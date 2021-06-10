e925160c0fe01a493850638caabbc5756694295b
Sergey <redmoloch@yandex.ru>
2021-06-10 18:52:44 +0300

    added OFFER_COUNT to data.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 import {getMockData} from './modules/get-mock-data.js';
-
-const SIMILAR_OFFER_COUNT = 10;
+import {SIMILAR_OFFER_COUNT} from './modules/data.js'
 
 const offers = getMockData(SIMILAR_OFFER_COUNT);
 offers; // чтоб eslint не ругался, потом удалю
--- js/modules/data.js
+++ js/modules/data.js
@@ -1,3 +1,4 @@
+const SIMILAR_OFFER_COUNT = 10;
 const START_LAT = 35.65000;
 const FINISH_LAT = 35.70000;
 const START_LNG = 139.70000;
@@ -18,4 +19,4 @@ const protoPhotos = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
 ];
 
-export {START_LAT, FINISH_LAT, START_LNG, FINISH_LNG, AFTER_POINT, PHOTO_COUNT, MIN_PRICE, MAX_PRICE, MAX_ROOMS, MAX_GUESTS, types, times, protoFeatures, protoPhotos};
+export {SIMILAR_OFFER_COUNT, START_LAT, FINISH_LAT, START_LNG, FINISH_LNG, AFTER_POINT, PHOTO_COUNT, MIN_PRICE, MAX_PRICE, MAX_ROOMS, MAX_GUESTS, types, times, protoFeatures, protoPhotos};
