5fb29934685fb310590a9dc3837ac41563caa595
Sergey <redmoloch@yandex.ru>
2021-06-19 00:00:35 +0300

    added form - began validation

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import { generateSimilarOfferCards } from './modules/generate-similar-offer-cards.js';
 import { changeFormCondition } from './modules/change-form-condition.js';
+import {form} from './modules/form.js';
 
 const offerElements = generateSimilarOfferCards();
 
@@ -10,3 +11,4 @@ canvas.append(offerElements[4]);
 // Напишите функцию, которая будет переводить страницу в активное состояние.
 changeFormCondition('.ad-form', 'ad-form--disabled', false);
 changeFormCondition('.map__filters', 'ad-form--disabled', false);
+form();
---
+++ js/modules/form.js
@@ -0,0 +1,69 @@
+const form = () => {
+  const formField = document.querySelector('.ad-form');
+  // const titleField = formField.querySelector('#title'); // так и не понял на что его валидировать в js, если с длиной справляются атрибуты в html
+  const priceField = formField.querySelector('#price');
+  const typeField = formField.querySelector('#type');
+  const roomNumber = formField.querySelector('#room_number');
+  const capacity = formField.querySelector('#capacity');
+  const optionCapacitys = capacity.querySelectorAll('option');
+
+  const typeAndPrice = {
+    bungalow: 0,
+    flat: 1000,
+    hotel: 3000,
+    house: 5000,
+    palace: 10000,
+  };
+
+  const roomsAndGuests = {
+    '1': ['1'],
+    '2': ['1', '2'],
+    '3': ['1', '2', '3'],
+    '100': ['0'],
+  };
+
+
+  const changePriceByType = () => {
+    typeField.addEventListener('change', (evt) => {
+      const minPrice = typeAndPrice[evt.target.value];
+      priceField.setAttribute('min', minPrice);
+      priceField.setAttribute('placeholder', minPrice);
+    });
+  };
+
+
+  const hideOptions = () => optionCapacitys.forEach((option) => option.style.display = 'none');
+
+  const showOptions = (array) => {
+    array.forEach((number) => {
+      optionCapacitys.forEach((option) => {
+        option.removeAttribute('selected');
+        if (option.value === number) {
+          option.style.display = 'block';
+        }
+      });
+    });
+  };
+
+  const getSelectedToOption = (number) => {
+    optionCapacitys.forEach((option) => {
+      if (option.value === number) {
+        option.setAttribute('selected', true);
+      }
+    });
+  };
+
+  const changeGuestsByRooms = () => {
+    roomNumber.addEventListener('change', (evt) => {
+      hideOptions();
+      const hideGuestNumbers = roomsAndGuests[evt.target.value];
+      showOptions(hideGuestNumbers);
+      getSelectedToOption(hideGuestNumbers[0]);
+    });
+  };
+
+  changePriceByType();
+  changeGuestsByRooms();
+};
+
+export {form};
