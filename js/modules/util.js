b4c832964adbf44c4c4f1dc4bdfe731e107ac561
Sergey <redmoloch@yandex.ru>
2021-07-08 20:01:08 +0300

    almost forgot / final version 2.3

--- js/modules/util.js
+++ js/modules/util.js
@@ -15,7 +15,7 @@ const changeBtnCondition = (btn, activize = false) => {
 // Функция взята из интернета и доработана
 // Источник - https://www.freecodecamp.org/news/javascript-debounce-example
 
-function debounce (callback, timeoutDelay = 500) {
+const debounce = (callback, timeoutDelay = 500) => {
   // Используем замыкания, чтобы id таймаута у нас навсегда приклеился
   // к возвращаемой функции с setTimeout, тогда мы его сможем перезаписывать
   let timeoutId;
@@ -31,6 +31,6 @@ function debounce (callback, timeoutDelay = 500) {
     // Таким образом цикл «поставить таймаут - удалить таймаут» будет выполняться,
     // пока действие совершается чаще, чем переданная задержка timeoutDelay
   };
-}
+};
 
 export {changeBtnCondition, debounce};
